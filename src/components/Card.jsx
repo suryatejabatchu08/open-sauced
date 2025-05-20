e308c37616b372a5a727f55942139a3c25774b1c
Surya Teja <152494188+suryatejabatchu08@users.noreply.github.com>
2025-05-20 08:05:59 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,6 @@ import {FittedCard, DisabledCard, Card} from "../styles/Card";
 
 function Cards({fitted, disabled, children}) {
   return fitted ? <FittedCard data-testid="fitted-card">{children}</FittedCard> :
-         disabled ? <DisabledCard data-testid="card">{children}</DisabledCard> : <Card data-testid="card">{children}</Card>;
+         disabled ? <DisabledCard data-testid="card">{Parents}</DisabledCard> : <Card data-testid="card">{children}</Card>;
 }
 export default Cards;
